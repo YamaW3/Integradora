863d484fc574ba197641c59a87dc7301047e01e7
Yoselin <ayoselin706@gmail.com>
2024-11-22 13:44:02 -0600

    responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,7 @@ window.addEventListener('scroll', function() {
     }  
 });
 
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
